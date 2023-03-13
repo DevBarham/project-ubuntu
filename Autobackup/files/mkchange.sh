8d87492513a3547efbb71ba23090ed48c572ed6e
DevBarham <saheedibrahimdamilare.com>
2023-03-13 12:01:25 +0100

    updating assignments

---
+++ Autobackup/files/mkchange.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+  GNU nano 6.2                                                          mkchange.sh *                                                                 
+#!/bin/bash
+mv -v /home/Autobackup/files/file1.txt /home/Autobackup/files/newest1.txt
+pdftotxt/home/Autobackup/files/file1.pdf /home/Autobackup/files/converted.txt
+
